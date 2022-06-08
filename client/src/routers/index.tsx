9899f07c68cb2429d154deedb2daa6a9d6e40474
stibenvelez <stibenvelez@hotmail.com>
2022-06-08 09:36:13 -0500

    last changes

--- client/src/routers/index.tsx
+++ client/src/routers/index.tsx
@@ -20,17 +20,6 @@ import PageContact from "containers/PageContact/PageContact";
 import PageAbout from "containers/PageAbout/PageAbout";
 import PageSignUp from "containers/PageSignUp/PageSignUp";
 import PageLogin from "containers/PageLogin/PageLogin";
-import PageSubcription from "containers/PageSubcription/PageSubcription";
-import PageAddListing1 from "containers/PageAddListing1/PageAddListing1";
-import PageAddListing2 from "containers/PageAddListing1/PageAddListing2";
-import PageAddListing3 from "containers/PageAddListing1/PageAddListing3";
-import PageAddListing4 from "containers/PageAddListing1/PageAddListing4";
-import PageAddListing5 from "containers/PageAddListing1/PageAddListing5";
-import PageAddListing6 from "containers/PageAddListing1/PageAddListing6";
-import PageAddListing7 from "containers/PageAddListing1/PageAddListing7";
-import PageAddListing8 from "containers/PageAddListing1/PageAddListing8";
-import PageAddListing9 from "containers/PageAddListing1/PageAddListing9";
-import PageAddListing10 from "containers/PageAddListing1/PageAddListing10";
 import PageHome2 from "containers/PageHome/PageHome2";
 import ListingRealEstateMapPage from "containers/ListingRealEstatePage/ListingRealEstateMapPage";
 import ListingRealEstatePage from "containers/ListingRealEstatePage/ListingRealEstatePage";
@@ -71,28 +60,16 @@ export const pages: Page[] = [
     { path: "/account-password", component: AccountPass },
     { path: "/account-savelists", component: AccountSavelists },
     { path: "/account-billing", component: AccountBilling },
-    { path: "/add-listing-1", component: PageAddListing1 },
-    { path: "/add-listing-2", component: PageAddListing2 },
-    { path: "/add-listing-3", component: PageAddListing3 },
-    { path: "/add-listing-4", component: PageAddListing4 },
-    { path: "/add-listing-5", component: PageAddListing5 },
-    { path: "/add-listing-6", component: PageAddListing6 },
-    { path: "/add-listing-7", component: PageAddListing7 },
-    { path: "/add-listing-8", component: PageAddListing8 },
-    { path: "/add-listing-9", component: PageAddListing9 },
-    { path: "/add-listing-10", component: PageAddListing10 },
     //
     { path: "/contact", component: PageContact },
     { path: "/about", component: PageAbout },
     { path: "/signup", component: PageSignUp },
     { path: "/login", component: PageLogin },
-    { path: "/subscription", component: PageSubcription },
     //
     //
     { path: "/venta", component: SalesPropertiesPage },
     { path: "/arriendo", component: SalesPropertiesPage },
     { path: "/property/:id", component: PropertyDetailPage },
-    //{ path: "/admin", component: AdminPage },
 ];
 
 const Routes = () => {
--- client/src/routers/types.ts
+++ client/src/routers/types.ts
@@ -31,21 +31,7 @@ export interface LocationStates {
     "/account-savelists"?: {};
     "/account-password"?: {};
     "/account-billing"?: {};
-    //
-    "/blog"?: {};
-    "/blog-single"?: {};
-    //
-    "/add-listing-1"?: {};
-    "/add-listing-2"?: {};
-    "/add-listing-3"?: {};
-    "/add-listing-4"?: {};
-    "/add-listing-5"?: {};
-    "/add-listing-6"?: {};
-    "/add-listing-7"?: {};
-    "/add-listing-8"?: {};
-    "/add-listing-9"?: {};
-    "/add-listing-10"?: {};
-    //
+
     "/author"?: {};
     "/search"?: {};
     "/about"?: {};
